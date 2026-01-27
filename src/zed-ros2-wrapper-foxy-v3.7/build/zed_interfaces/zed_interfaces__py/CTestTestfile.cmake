62639ace504f53fea21fef67abefba31d02ed16c
Oğuzhan Eşen <oguzhan.esen@tum.de>
2026-01-27 23:30:36 +0100

    Remove src directory entirely

--- src/zed-ros2-wrapper-foxy-v3.7/build/zed_interfaces/zed_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/f1tenth/zed-ros2-wrapper-foxy-v3.7/build/zed_interfaces/zed_interfaces__py
-# Build directory: /home/f1tenth/zed-ros2-wrapper-foxy-v3.7/build/zed_interfaces/zed_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
